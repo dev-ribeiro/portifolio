3133a8ea7f5002206fbf975242ea0ad3f9967ef6
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-04-14 20:19:52 -0300

    add techs on page index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import style from "../styles/Home.module.css"
-import Header from "../components/Header"
+import Header from "../components/Header";
+import About from "../components/Home/About";
+import Techs from "../components/Home/Techs";
+
 
 function Home(){
     return (
         <div className={style.home}>
             <Header></Header>
+            <About></About>
+            <Techs></Techs>
         </div>
     )
 };
