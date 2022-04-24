52f98ea68afe05a8f22d0f3fbdd9aa8eb99f50f4
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-04-23 22:31:33 -0300

    test background-image

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-            <title>Seja bem vindo!</title>
-            <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-            <link rel='icon' href='https://www.flaticon.com/svg/vstatic/svg/3916/3916581.svg?token=exp=1650581044~hmac=6dd48f63c73442fe81e705add05a7189'></link>
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
