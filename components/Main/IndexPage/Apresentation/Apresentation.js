75385707cce5697818284040aaca975bd093454f
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-04-27 22:51:16 -0300

    test images

--- components/Main/IndexPage/Apresentation/Apresentation.js
+++ components/Main/IndexPage/Apresentation/Apresentation.js
@@ -1,12 +1,12 @@
-import style from '../../../styles/Main.module.css'
+import style from '../../../../styles/Main/IndexPage.module.css'
 
 function Apresentation(){
     return(
-        <div className={style.container}>
+        <section className={style.apresentation}>
             <h2>&gt;&gt; Hello World</h2>
             <p>Sou o David. Estou estudando as tecnologias relacionadas ao desenvolvimento WEB. Sou um entusiasta no segmento de tecnologia e tenho bastante interesse na área. Atuo na área do desenvolvimento WEB, na criação de páginas estáticas e SPA (Single Page Application). Confiram meus projetos e meu contato no botão abaixo.</p>
             <button className={style.button}>Saiba mais</button>
-        </div>
+        </section>
     )
 };
 
