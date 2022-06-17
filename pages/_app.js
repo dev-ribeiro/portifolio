f637bb8aebd7562c56c1c36d0f1209caf8eb99ce
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-06-17 09:29:50 -0300

    add stylesheet files in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import Head from 'next/head';
 import '../public/styles/globals.css';
+import '../public/styles/header.css';
+import '../public/styles/footer.css';
+import '../public/styles/home.css';
+import '../public/styles/projects.css';
+import '../public/styles/about.css';
 
 export default function MyApp({ Component, pageProps }) {
     return (
