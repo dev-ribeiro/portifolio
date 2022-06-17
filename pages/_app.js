e4a8f691f3e6c895dd6937872417d6e3acef0fa0
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-06-17 08:53:37 -0300

    adjust css file in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import '../public/global/globals.css';
-import '../public/global/header.css'
 
 export default function MyApp({ Component, pageProps }) {
     return (
