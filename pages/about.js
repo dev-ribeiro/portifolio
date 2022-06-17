72e1bbd1d02245fe73cd5bb1d54496dd0a2e4287
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-06-17 09:16:20 -0300

    create initial content to pages about and projects for tests Link component of Next

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Header from "../src/components/Header/Header";
+
+export default function About(){
+    return(
+        <div>
+            <Header/>
+            About
+        </div>
+    )
+}
---
+++ pages/projects.js
@@ -0,0 +1,10 @@
+import Header from "../src/components/Header/Header";
+
+export default function Projects(){
+    return(
+        <div>
+            <Header/>
+            Projects
+        </div>
+    )
+}
