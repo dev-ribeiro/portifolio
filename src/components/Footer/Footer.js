621e52e5def7067230696e295c45b84a1f2d7400
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-06-17 09:23:40 -0300

    create initial footer component code

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+export default function Footer(props){
+    return(
+        <footer>
+            <section>
+                <h3>Contato:</h3>
+                <ul>
+                    <li>E-mail</li>
+                    <li>LinkedIn</li>
+                    <li>GitHub</li>
+                </ul>
+            </section>
+        </footer>
+    )
+}
