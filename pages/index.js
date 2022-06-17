543c472f74e43c4dae7931eb31d891ab78a40c2c
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-06-17 09:25:19 -0300

    create initial structure of home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Header from "../src/components/Header/Header";
+import Footer from "../src/components/Footer/Footer";
 
 export default function Home() {
     return (
         <>
             <Header/>
-            Hello world
+            <main>
+                <h2>Hello world</h2>
+                <p>Texto de apresentação</p>
+            </main>
+            <Footer/>
         </>
     )
 };
