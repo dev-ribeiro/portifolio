a9459a26634a9a60f8780a5ad9911fb9db78cab8
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-06-25 16:21:42 -0300

    create main content of home page

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import Footer from "../src/components/Footer/Footer";
 export default function Home() {
     return (
         <>
-            <Header/>
-            <main>
-                <h2>Hello world</h2>
-                <p>Texto de apresentação</p>
+            <Header />
+            <main className="home">
+                <div className="homeContent">
+                    <h2>&gt;&gt;Hello world</h2>
+                    <p className="apresentation">Sou David Ribeiro. Estudo programação e outros conteúdos da área de tecnologia, desde novembro de 2021, mas tenho afinidade com computadores desde a adolescência. Atualmente, estou cursando uma graduação na área e também complemento o estudo da programação com cursos livres e muita prática, com exercícios e projetos.</p>
+                </div>
             </main>
-            <Footer/>
+            <Footer />
         </>
     )
 };
