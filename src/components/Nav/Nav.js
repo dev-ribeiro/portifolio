308f5801e1cf15f92e8be54863c7c9f8df6e4879
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-06-29 20:00:17 -0300

    adjust nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,10 @@
 import style from "./Nav.module.css";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function Nav(props) {
 
-    const [show, setShow] = useState(false);
-
     return (
-        <nav
-            onClick={() => { setShow(true) }}
-            onMouseLeave={() => { setShow(false) }}
-            className={show == false ? `${style.navigation} ${style.initialNav}` : `${style.navigation} ${style.showNav}`}
-        >
+        <nav className={style.navigation}>
             <Link href="/"><a className={style.linkName}>Home</a></Link>
             <Link href="/projects"><a className={style.linkName}>Projetos</a></Link>
             <Link href="/about"><a className={style.linkName}>Sobre</a></Link>
