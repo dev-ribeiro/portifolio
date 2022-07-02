07400e703a2cd37e2c2616fa35f1470fa24379b6
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-07-02 12:04:35 -0300

    adjust footer contact and icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,19 @@
+import Link from "next/link";
 import style from "./Footer.module.css";
 
-export default function Footer(props){
-    return(
+export default function Footer(props) {
+    return (
         <footer className={style.footer}>
             <section className={style.contact}>
                 <h3>Contato:</h3>
-                <ul>
-                    <li>E-mail</li>
-                    <li>LinkedIn</li>
-                    <li>GitHub</li>
-                </ul>
+                <div>
+                    <img src="/assets/icons/footer/linkedin.png"></img>
+                    <Link href="https://www.linkedin.com/in/david-ribeiro-21297322b"><a>LinkedIn</a></Link>
+                </div>
+                <div>
+                    <img src="/assets/icons/footer/github.png"></img>
+                    <Link href="https://github.com/dev-ribeiro"><a>GitHub</a></Link>
+                </div>
             </section>
         </footer>
     )
