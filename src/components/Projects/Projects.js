0a835dd81b8406f6dd1b64d801fbf684460b4376
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-07-02 14:20:31 -0300

    create content of page project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,22 @@
+
 import style from "./Projects.module.css";
 
-export default function Projects(){
-    return(<main className={style.projects}>
-        <div>
-            <h3>Simulador de investimentos</h3>
-            <p>Uma API que permite calcular rendimentos de aplicação e buscar opções de investimentos.</p>
-        </div>
+export default function Projects() {
+
+    return (<main className={style.projects}>
+                <div>
+                    <h3>Simulador de investimentos</h3>
+                    <p>Uma API que permite calcular rendimentos de aplicação e buscar opções de investimentos.</p>
+                    <a href="https://dev-ribeiro.github.io/investments-simulator/"><button>Acessar</button></a>
+                </div>
+                <div>
+                    <h3>Bingo</h3>
+                    <p>Um site que vai reproduzir um sistema de um bingo, com HTML, CSS e Javascript "puro".</p>
+                    <a href="https://dev-ribeiro.github.io/bingo-online/"><button>Acessar</button></a>
+                </div>
+                <div>
+                    <h3></h3>
+                    <p></p>
+                </div>
     </main>)
 }
