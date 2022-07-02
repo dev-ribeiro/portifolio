9ad0622f1c47cc7dc6e1a84adb0f466c61708ec5
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-07-02 14:21:53 -0300

    add footer on about page

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,12 @@
 import Header from "../src/components/Header/Header";
+import Footer from "../src/components/Footer/Footer";
 
 export default function About(){
     return(
         <div>
             <Header/>
-            About
+            <h3>Em desenvolvimento</h3>
+            <Footer/>
         </div>
     )
 }
