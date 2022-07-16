560ec770193819632432e16c4ba8ebfa35406dd1
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-07-16 20:00:05 -0300

    begin - rework of portifolio

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import Header from "../src/components/Header/Header";
-import Home from "../src/components/Home/Home";
-import Footer from "../src/components/Footer/Footer";
+
 
 export default function HomePage() {
     return (
         <>
-            <Header />
-            <Home />
-            <Footer />
+            Hello World
         </>
     )
 };
