c7547c0582cb8db35002ff5e00cd1ad2892d1d83
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-07-17 12:56:39 -0300

    adjust index page and globals css

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Header from "../src/components/GeneralComponents/Header/Header";
+import Menu from "../src/components/GeneralComponents/Menu/Menu";
+import ApresentationInitial from "../src/components/HomeScreen/ApresentationInitial/ApresentationInitial";
 
 export default function HomePage() {
     return (
         <>
             <Header/>
+            <main>
+                <Menu/>
+                <ApresentationInitial/>
+            </main>
         </>
     )
 };
