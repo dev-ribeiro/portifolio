b32d56e6287576251a46618e2a98a9c65cd17644
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-07-24 08:37:18 -0300

    replace Menu component for MainContent to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Menu from "../src/components/GeneralComponents/Menu/Menu";
 import Header from "../src/components/GeneralComponents/Header/Header";
 import MainContent from "../src/components/HomeScreen/MainContent/MainContent";
 
 export default function HomePage() {
     return (
         <>
+            <Menu/>
             <Header/>
             <MainContent/>
         </>
